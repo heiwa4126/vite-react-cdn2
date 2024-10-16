68ee704fddd8bfa9112014e331a5ecb5c2f40473
heiwa4126 <heiwa4126@gmail.com>
2024-10-16 13:28:48 +0900

    update

--- src/Cowsay.tsx
+++
@@ -1,5 +0,0 @@
-import { say } from "cowsay";
-
-export function Cowsay({ text }: { text: string }) {
-	return <pre>{say({ text })}</pre>;
-}
--- vite.config.ts
+++ vite.config.ts
@@ -10,15 +10,15 @@ export default defineConfig({
 			modules: [
 				"react",
 				"react-dom",
-				{
-					name: "cowsay",
-					var: "cowsay",
-					// path: "build/cowsay.umd.min.js",
-					// jsDelivrの場合はこれで終わり
-					// UNPKGの場合
-					prodUrl: "https://unpkg.com/{name}@{version}/{path}",
-					path: "build/cowsay.umd.js", //UNPKGにはminがないらしい
-				},
+				// {
+				// 	name: "cowsay",
+				// 	var: "cowsay",
+				// 	// path: "build/cowsay.umd.min.js",
+				// 	// jsDelivrの場合はこれで終わり
+				// 	// UNPKGの場合
+				// 	prodUrl: "https://unpkg.com/{name}@{version}/{path}",
+				// 	path: "build/cowsay.umd.js", //UNPKGにはminがないらしい
+				// },
 			],
 		}),
 	],
